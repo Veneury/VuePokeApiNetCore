60065ec33a2171fb1b7fa89202e95c4ae2771b00
Veneury <deversnolasco@gmail.com>
2020-02-03 22:17:04 -0400

    VueApp

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <h1>Poke App</h1>
+    <PokemonList 
+      :imageUrl="imageUrl" 
+      :apiUrl="apiUrl"
+      @setPokemonUrl="setPokemonUrl" />
+    <PokemonDetail 
+      v-if="showDetail"
+      :pokemonUrl="pokemonUrl"
+      :imageUrl="imageUrl"
+      @closeDetail="closeDetail" />
+  </div>
+</template>
+
+<script>
+  import PokemonList from './PokemonList.vue';
+  import PokemonDetail from './PokemonDetail.vue';
+
+  export default {
+    data: () => {
+      return {
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+        apiUrl: 'https://localhost:44316/api/PokeApi/',
+        pokemonUrl: '',
+        showDetail: false
+      }
+    },
+    components: {
+      PokemonList,
+      PokemonDetail
+    },
+    methods: {
+      setPokemonUrl(url) {
+        this.pokemonUrl = url;
+        this.showDetail = true;
+      },
+      closeDetail() {
+        this.pokemonUrl = '';
+        this.showDetail = false;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css?family=Acme');
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    width: calc(100% - 20px);
+    min-height: calc(100vh - 20px);
+     background: linear-gradient(to right, #00b4db, #0083b0);
+
+    font-family: 'Acme', arial;
+    font-size: 1rem;
+    font-weight: normal;
+  }
+
+  h1 { color: #efefef; }
+</style>
